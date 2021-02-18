23ac9ec0d982ba3d0facc7087bcd647569e41e1f
kcrammer <kcrammer@gmail.com>
2021-02-17 22:02:30 -0500

    add files

---
+++ index.js
@@ -0,0 +1,66 @@
+const Manager= require("./lib/Manager")
+const Engineer= require("./lib/Engineer")
+const Intern= require("./lib/Intern")
+
+const inquirer= require("inquirer")
+const fs= require("fs")
+const html= require("./src/html")
+const managerCard= require ("./src/managerCard")
+const engineerCard= require ("./src/engineerCard")
+const internCard= require ("./src/internCard")
+
+
+const employeeDB=[]
+
+let questions=[]
+
+function askQuestion (){
+
+      questions =[{ 
+        type: "list",
+        message: "Choose from the following employee options:",
+        choices: ["Add Manager", "Add Engineer", "Add Intern", "Exit"],
+        name: "choice"
+
+      }]
+
+      inquirer.prompt(questions)
+      .then(function(response){
+              switch(response.choice){
+                  case "Add Manager":
+                      addManager()
+                      break
+              }
+      })
+}
+function addManager(){
+    questions=[{
+        type: "input",
+        message: "What is the employee's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the employee's ID number?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the employee's email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the employee's office number?",
+        name: "officeNumber"
+    }
+]
+    inquirer.prompt(questions)
+    .then(function(response){
+        const newManager= new Manager(response.name,response.id,response.email,response.officeNumber)
+        employeeDB.push(newManager)
+        console.log(employeeDB)
+        askQuestion()
+    })
+}
+askQuestion()
---
+++ src/managerCard.js
@@ -0,0 +1,19 @@
+function card(response){
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${response.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+      <p class="card-text">
+          ID: ${response.id}
+          <br>
+          Email: ${response.email}
+          <br>
+          Office number: ${response.officeNumber}
+      </p>
+    </div>
+  </div>
+    `
+}
+
+module.exports = card
